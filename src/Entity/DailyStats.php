23e377cd34de69ba0a424703d5784c2900eef6d6
github-ci <you@example.com>
2025-06-18 11:25:09 +0800

    daily update

--- src/Entity/DailyStats.php
+++ src/Entity/DailyStats.php
@@ -4,12 +4,9 @@ namespace YouzanApiDataBundle\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
-use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
-use Tourze\DoctrineTimestampBundle\Attribute\UpdateTimeColumn;
+use Tourze\DoctrineTimestampBundle\Traits\TimestampableAware;
 use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
 use Tourze\EasyAdmin\Attribute\Column\ListColumn;
-use Tourze\EasyAdmin\Attribute\Filter\Filterable;
 use YouzanApiBundle\Entity\Account;
 use YouzanApiBundle\Entity\Shop;
 use YouzanApiDataBundle\Repository\DailyStatsRepository;
@@ -22,6 +19,7 @@ use YouzanApiDataBundle\Repository\DailyStatsRepository;
 #[ORM\UniqueConstraint(name: 'uk_account_shop_day', columns: ['account_id', 'shop_id', 'current_day'])]
 class DailyStats
 {
+    use TimestampableAware;
     #[ListColumn(order: -1)]
     #[ExportColumn]
     #[ORM\Id]
@@ -57,21 +55,6 @@ class DailyStats
 
     #[ORM\Column(type: 'decimal', precision: 10, scale: 2, options: ['comment' => '不含退款的支付金额，单位：元'])]
     private string $excludeCashbackRefundedAmt = '0.00';
-
-    #[Filterable]
-    #[IndexColumn]
-    #[ListColumn(order: 98, sorter: true)]
-    #[ExportColumn]
-    #[CreateTimeColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
-    private ?\DateTimeInterface $createTime = null;
-
-    #[UpdateTimeColumn]
-    #[ListColumn(order: 99, sorter: true)]
-    #[Filterable]
-    #[ExportColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '更新时间'])]
-    private ?\DateTimeInterface $updateTime = null;
 
     public function getId(): ?int
     {
@@ -175,25 +158,4 @@ class DailyStats
     {
         $this->excludeCashbackRefundedAmt = $excludeCashbackRefundedAmt;
         return $this;
-    }
-
-    public function setCreateTime(?\DateTimeInterface $createdAt): void
-    {
-        $this->createTime = $createdAt;
-    }
-
-    public function getCreateTime(): ?\DateTimeInterface
-    {
-        return $this->createTime;
-    }
-
-    public function setUpdateTime(?\DateTimeInterface $updateTime): void
-    {
-        $this->updateTime = $updateTime;
-    }
-
-    public function getUpdateTime(): ?\DateTimeInterface
-    {
-        return $this->updateTime;
-    }
-}
+    }}
